d16fbdde3140d93998a1d287a624fb90241815f8
Amarjot Parmar <amarjotparmar098@gmail.com>
2018-10-24 22:27:07 +1300

    setup list

---
+++ app/src/main/java/com/parmar/amarjot/android_imagelocationfinder/CustomListview.java
@@ -0,0 +1,60 @@
+package com.parmar.amarjot.android_imagelocationfinder;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+public class CustomListview extends ArrayAdapter <String>{
+
+    private String [] recipe_title;
+    private Integer [] recipe_image_id;
+
+    private Activity context;
+
+    public CustomListview(@NonNull Context context, String [] _recipeName, Integer [] _recipeID) {
+        super(context, R.layout.custom_listview_layout, _recipeName);
+
+        this.context= (Activity) context;
+        this.recipe_title=_recipeName;
+        this.recipe_image_id=_recipeID;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View r = convertView;
+        ViewHolder viewHolder;
+        if (r == null) {
+            LayoutInflater layoutInflater = context.getLayoutInflater();
+            r = layoutInflater.inflate(R.layout.custom_listview_layout, null, true);
+            viewHolder = new ViewHolder(r);
+            r.setTag(viewHolder);
+        }
+        else {
+            viewHolder = (ViewHolder) r.getTag();
+        }
+
+        viewHolder.textViewName.setText(recipe_title[position]);
+        viewHolder.imageView.setImageResource(R.drawable.common_google_signin_btn_icon_dark);//recipe_image_id[position]);
+
+        return r;
+    }
+
+    class ViewHolder {
+        TextView textViewName;
+        CircularImageView imageView;
+        ViewHolder (View v) {
+            textViewName = v.findViewById(R.id.textTitle);
+            imageView = v.findViewById(R.id.imageView);
+            textViewName.setTextSize(20);
+        }
+    }
+}
--- app/src/main/java/com/parmar/amarjot/android_imagelocationfinder/MainActivity.java
+++ app/src/main/java/com/parmar/amarjot/android_imagelocationfinder/MainActivity.java
@@ -6,40 +6,68 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
+import static com.parmar.amarjot.android_imagelocationfinder.R.array.name;
+
 public class MainActivity extends AppCompatActivity{
+
+    // Used to display recipes stored in local db.
+    ListView list;
 
     private static final String TAG = "MainActivity";
     private static final int ERROR_DIALOG_REQUEST = 9001;
+
+    String [] landmarkNames = new String[2];
+    Integer [] imageID = new Integer[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        if(isServicesOK()){
-//            init();
-//        }
+        isServicesOK();
+        setupList();
+    }
 
-        goToMaps();
+    private void setupList() {
+        // load data from resources into arrays
+        loadArrays();
+
+        // Make list using array
+
+        //goToMaps(0);
+        list= findViewById(R.id.listView);
+        CustomListview customListview = new CustomListview(this, landmarkNames, imageID);
+        list.setAdapter(customListview);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                goToMaps(i);
+            }
+        });
+    }
+
+    private void loadArrays() {
+        // Read data from string array
+        // Load into array
+        landmarkNames = getResources().getStringArray(R.array.name);
 
     }
 
-    private void init() {
-        Button btnMap = (Button) findViewById(R.id.btnMapbutton);
 
-    }
-
-    public void goToMaps(){
+    public void goToMaps(int landmarkIndex){
         Intent intent = new Intent(this, MapActivity.class);
         startActivity(intent);
     }
 
+    // Makes sure app can communicate with GoogleMaps
     public boolean isServicesOK(){
         Log.d(TAG, "isServicesOK: checking google services version");
 
